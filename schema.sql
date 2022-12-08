6e02ec0688f4fc2f0a38f9a883972c812affbf51
ecekpo <ememekpo@yahoo.com>
2022-12-08 15:56:31 +0100

    Add modified schema file

--- schema.sql
+++ schema.sql
@@ -25,3 +25,9 @@ CREATE TABLE Species (
     name varchar(255) NOT NULL
 );
 
+//Modifying animals table
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id);
+ALTER TABLE animals ADD owner_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
