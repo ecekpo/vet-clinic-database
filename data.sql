af1678abf7d5585933aa3ab5e23337cc322f0b58
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-12-12 14:02:45 +0000

    Added index for animal, visits and email for performance

--- data.sql
+++ data.sql
@@ -50,3 +50,7 @@ INSERT INTO visits (animal_id, vets_id, date_of_visit) SELECT * FROM (SELECT id 
 
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+CREATE INDEX idx_animal_id ON visits (animal_id);
+CREATE INDEX idx_vets_id ON visits (vets_id);
+CREATE INDEX email ON owners (email);
